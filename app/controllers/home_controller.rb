da0e5ac3b0906d8269688b4396d73249d1d61597
udayaratna <udaya.ratna@gmail.com>
2016-08-07 23:43:28 -0700

    added restaurant list to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
  def index
      if user_signed_in?
+      if current_user.Role == 'owner'
+        redirect_to user_restaurants_path(current_user.id)
+      else
        redirect_to categories_path
+     end
      else
        redirect_to new_user_session_path 
      end
