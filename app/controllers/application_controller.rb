f69a5d293ee87f7b28a4a89ec20c5ddc59b01f2e
udayaratna <udaya.ratna@gmail.com>
2016-08-09 05:59:40 -0700

    added gem cancan and included in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
+ # For APIs, you may want to use :null_session instead.
+before_filter :configure_permitted_parameters, if: :devise_controller?
+#check_authorization
   protect_from_forgery with: :exception
+
+rescue_from CanCan::AccessDenied do |exception|
+      redirect_to root_url, :alert => exception.message
+end
+
 protected
       def configure_permitted_parameters
-          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:Name, :email, :password, :Role) }
-          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:Name, :email, :password, :current_password, :Role) }
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:Name, :email, :password, :Role])
+          devise_parameter_sanitizer.permit(:account_update, keys: [:Name, :email, :password, :current_password, :Role])
 end
+
 
 
 end
